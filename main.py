93b21d41685cd643054eb9dc10f142e8718941c3
abavi <51476525+abavi@users.noreply.github.com>
2024-07-03 23:03:18 +0100

    added open_file function and modified main file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        
-        # Count the number of words using len        
-        words = file_contents.split()
-        print(f"{len(words)} words found in the document!")
-                
+    path_location = "books/frankenstein.txt"
+    file_contents = open_file(path_location)
+    
+    # Count the number of words using len        
+    words = file_contents.split()
+    print(f"{len(words)} words found in the document!")
+
+def open_file(path_to_location):
+    with open(path_to_location) as f:
+        return f.read()
+
 main()
