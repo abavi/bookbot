1d9c3d39087ac20d4b0e7602b43c0fdb99ec9ca4
abavi <51476525+abavi@users.noreply.github.com>
2024-07-03 23:28:16 +0100

    added function to count characters

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ def main():
     # Count the number of words using len        
     words = file_contents.split()
     print(f"{len(words)} words found in the document!")
+    print(f"The character count is: {count_characters(file_contents)}")
 
 def open_file(path_to_location):
     with open(path_to_location) as f:
         return f.read()
+    
+def count_characters(file_contents):
+    character_count = {} # Empty dict to count characters CHAR:COUNT
+
+    words = file_contents.lower().split() # Create list of words
+
+    # Loop through the words and then letters and add them to the dict
+    for word in words:
+        for letter in word:
+            if letter in character_count:
+                character_count[letter] += 1
+            else:
+                character_count[letter] = 1
+    
+    return character_count
 
 main()
